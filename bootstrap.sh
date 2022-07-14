5fdae9ce08423fa8ca120db05738fbf1f45249ac
Fedor Semenov <semenovf@users.noreply.github.com>
2022-07-14 09:49:19 +0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
 BASE_URL=https://raw.githubusercontent.com/semenovf/scripts/master
-PORTABLE_TARGET_BASE_URL=https://raw.githubusercontent.com/semenovf/portable-target/master/cmake/v2
 BAK_SUFFIX=`date +%Y_%m_%d_%H_%M_%S`
 
 SCRIPTS="build.sh \
     build-windows.cmd"
 
 CONFIGS=".gitignore \
-    .travis.yml \
     appveyor.yml \
     codecov.yml"
 
@@ -49,32 +47,3 @@ for f in $CONFIGS ; do
     curl_copy $BASE_URL/configs/${f} $f
     [ $LAST_STATUS -ne 0 ] && exit 1
 done
-
-######################################################
-# portable-target
-######################################################
-#mkdir -p cmake/portable-target/actions
-#
-#PORTABLE_TARGET="ColouredMessage.cmake \
-#    Functions.cmake \
-#    PortableTarget.cmake"
-#
-#PORTABLE_TARGET_ACTIONS="add_executable.cmake \
-#    add_library.cmake \
-#    compile_options.cmake \
-#    definitions.cmake \
-#    include_directories.cmake \
-#    link_libraries.cmake \
-#    link_qt5_components.cmake \
-#    properties.cmake \
-#    sources.cmake"
-#
-#for f in $PORTABLE_TARGET ; do
-#    curl_copy $PORTABLE_TARGET_BASE_URL/${f} cmake/portable-target/$f
-#    [ $LAST_STATUS -ne 0 ] && exit 1
-#done
-#
-#for f in $PORTABLE_TARGET_ACTIONS ; do
-#    curl_copy $PORTABLE_TARGET_BASE_URL/actions/${f} cmake/portable-target/actions/$f
-#    [ $LAST_STATUS -ne 0 ] && exit 1
-#done
